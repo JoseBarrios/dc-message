6f7b677f845927de3cc4714aa816ff0943a28324
Jose Barrios <jose@cognilab.com>
2017-07-02 22:20:10 -0700

    updated getters and setters, forgot to include sender and reciever

--- index.js
+++ index.js
@@ -28,14 +28,11 @@ class Message extends Multiple(Thing, CreativeWork){
   get messageAttachment() { return this.computed.messageAttachment; }
   set messageAttachment(value) { this.computed.messageAttachment = value; }
 
-  get dateSent() { return this.computed.dateSent; }
-  set dateSent(value) { this.computed.dateSent = value; }
+  get recipient() { return this.computed.recipient; }
+  set recipient(value) { this.computed.recipient = value; }
 
-  get dateReceived() { return this.computed.dateReceived; }
-  set dateReceived(value) { this.computed.dateReceived = value; }
-
-  get dateRead() { return this.computed.dateRead; }
-  set dateRead(value) { this.computed.dateRead = value; }
+  get sender() { return this.computed.sender; }
+  set sender(value) { this.computed.sender = value; }
 }
 
 module.exports = Message;
